de2c05045a75dd6557b2fe177aa637a8b6ff8675
tcoolguy1 <78965454+tcoolguy1@users.noreply.github.com>
2022-01-24 18:07:54 +0530

    Add files via upload

---
+++ chair_main.js
@@ -0,0 +1,49 @@
+img = "";
+status1="";
+object = [];
+function preload() {
+    img = loadImage("https://i.postimg.cc/2j2R61KT/chair.jpg");
+}
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects"; 
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+    status1=true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error,results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    object = results;
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420); 
+    if(status1 !="")
+    {
+        for(i = 0; i < object.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+
+            fill("#FF0000");
+            percent = floor(object[i].confidence*100);
+            text(object[i].label + " "+ percent + "%",object[i].x, object[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(object[i].x,object[i].y,object[i].width,object[i].height);
+        }
+    }   
+
+}
+
+function back() {
+    window.location = "index.html";
+}
